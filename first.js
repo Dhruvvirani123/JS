467760ef359b39ad8854e28c8a4c5782341c0915
Dhruvvirani123 <viranidhruv002@gmail.com>
2023-09-13 10:09:05 +0530

    first commit

---
+++ first.js
@@ -0,0 +1,38 @@
+let num = 2+3
+console.log("Hello World")
+console.log(2+2)
+console.log(num*4)
+console.log(num)
+let username = 'Dhruv Patel'// Store String "" '' any use in String
+console.log(username)
+
+let radius = 5
+const pi = 3.14 // Value not change -> use const variable 
+let area
+
+radius = 6
+// pi = 3.22 error this program
+area = pi * radius * radius
+console.log(area) // output = 113.0399999999999999
+
+let user = 'Dhruv Virani'
+console.log(typeof user); // output = String
+
+let num1 = 7.9
+console.log(typeof num1); // output = number
+
+let num2 = 1.5e12
+console.log(num2) //output = 1500000000000
+
+let num3 = 100_00_00
+console.log(num3) //output = 1000000
+
+let num4 = -5/0
+console.log(num4); // Output = Infinity
+
+let num5 = -5/0
+console.log(Number.MAX_VALUE) //output = 1.7976934........
+
+let num6 = -5/0
+console.log(Number.MAX_VALUE * 10) //output = Infinity
+
